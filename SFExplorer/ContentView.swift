9253551b5027a3bdde09be9a9955e2a2bc30f060
zmeriksen <zmeriksen@gmail.com>
2019-07-12 14:40:54 -0500

    Initial Commit

---
+++ SFExplorer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SFExplorer
+//
+//  Created by Zach Eriksen on 7/12/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
